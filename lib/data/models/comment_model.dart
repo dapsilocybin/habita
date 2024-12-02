54c36906ce0d6c893b7bbbc8acf9fc6cb3511a96
Mostafa Mashayekhi <dapsilocybin@gmail.com>
2024-12-02 11:19:26 +0330

    Define mappers in comment_model.dart

--- lib/data/models/comment_model.dart
+++ lib/data/models/comment_model.dart
@@ -7,4 +7,24 @@ class CommentModel {
 
   CommentModel({required this.id, required this.createdAt, required this.content, required this.userId, required this.recordId});
 
+   Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'createdAt': createdAt,
+      'content': content,
+      'userId': userId,
+      'recordId': recordId,
+    };
+  }
+
+  static CommentModel fromMap(Map<String, dynamic> data) {
+    return CommentModel(
+      id: data['id'] as String,
+      createdAt: data['createdAt'] as DateTime,
+      content: data['content'] as String,
+      userId: data['userId'] as String,
+      recordId: data['recordId'] as String,
+    );
+  }
+
 }
