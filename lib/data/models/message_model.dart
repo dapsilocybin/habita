50ab6242f8293878e4541953cfe2c899ec164ab7
Mostafa Mashayekhi <dapsilocybin@gmail.com>
2024-12-02 11:03:35 +0330

    define mappers in message_model.dart

--- lib/data/models/message_model.dart
+++ lib/data/models/message_model.dart
@@ -6,4 +6,25 @@ class MessageModel {
   final String senderId;
 
   MessageModel({required this.id, required this.createdAt, required this.content, required this.chatId, required this.senderId});
+
+   Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'createdAt': createdAt,
+      'content': content,
+      'chatId': chatId,
+      'senderId': senderId,
+    };
+  }
+
+  static MessageModel fromMap(Map<String, dynamic> data) {
+    return MessageModel(
+      id: data['id'] as String,
+      createdAt: data['createdAt'] as DateTime,
+      content: data['content'] as String,
+      chatId: data['chatId'] as String,
+      senderId: data['senderId'] as String,
+    );
+  }
+  
 }
