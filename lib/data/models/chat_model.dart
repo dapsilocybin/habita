54261c87e11a70478f6b25b0e890e2eae3b9dace
Mostafa Mashayekhi <dapsilocybin@gmail.com>
2024-12-02 11:21:45 +0330

    Define mappers in chat_model.dart

--- lib/data/models/chat_model.dart
+++ lib/data/models/chat_model.dart
@@ -4,4 +4,20 @@ class ChatModel {
   final String habitId;
 
   ChatModel({required this.id, required this.createdAt, required this.habitId});
+
+   Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'createdAt': createdAt,
+      'habitId': habitId,
+    };
+  }
+
+  static ChatModel fromMap(Map<String, dynamic> data) {
+    return ChatModel(
+      id: data['id'] as String,
+      createdAt: data['createdAt'] as DateTime,
+      habitId: data['habitId'] as String,
+    );
+  }
 }
