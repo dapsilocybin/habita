26e39609aaccdefbbe5d756f4d0fb003e5787c77
Mostafa Mashayekhi <dapsilocybin@gmail.com>
2024-12-02 10:58:07 +0330

    define mappers in notification_model.dart

--- lib/data/models/notification_model.dart
+++ lib/data/models/notification_model.dart
@@ -7,4 +7,27 @@ class NotificationModel {
   final String userId;
 
   NotificationModel({required this.id, required this.createdAt, required this.type, required this.message, required this.isRead, required this.userId});
+
+   Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'createdAt': createdAt,
+      'type': type,
+      'message': message,
+      'isRead': isRead,
+      'userId': userId,
+    };
+  }
+
+  static NotificationModel fromMap(Map<String, dynamic> data) {
+    return NotificationModel(
+      id: data['id'] as String,
+      createdAt: data['createdAt'] as DateTime,
+      type: data['type'] as String,
+      message: data['message'] as String,
+      isRead: data['isRead'] as String,
+      userId: data['userId'] as String,
+    );
+  }
+  
 }
