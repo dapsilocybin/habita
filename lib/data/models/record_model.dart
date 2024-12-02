830e0f510258370d29a00ba761f71d7adf16c94e
Mostafa Mashayekhi <dapsilocybin@gmail.com>
2024-12-02 10:53:00 +0330

    define mappers in record_model.dart

--- lib/data/models/record_model.dart
+++ lib/data/models/record_model.dart
@@ -9,4 +9,30 @@ class RecordModel {
   final String userId;
 
   RecordModel({required this.id, required this.createdAt, required this.caption, required this.picture, required this.location, required this.likes, required this.habitId, required this.userId});
+
+   Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'createdAt': createdAt,
+      'caption': caption,
+      'picture': picture,
+      'location': location,
+      'likes': likes,
+      'habitId': habitId,
+      'userId': userId,
+    };
+  }
+
+  static RecordModel fromMap(Map<String, dynamic> data) {
+    return RecordModel(
+      id: data['id'] as String,
+      createdAt: data['createdAt'] as DateTime,
+      caption: data['caption'] as String,
+      picture: data['picture'] as String,
+      location: data['location'] as String,
+      likes: data['likes'] as int,
+      habitId: data['habitId'] as String,
+      userId: data['userId'] as String,
+    );
+  }
 }
