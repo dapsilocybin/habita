b165b20bf28c221c410c3195ea642a5d440cc326
Mostafa Mashayekhi <dapsilocybin@gmail.com>
2024-12-02 11:24:21 +0330

    Define mappers in category_model.dart

--- lib/data/models/category_model.dart
+++ lib/data/models/category_model.dart
@@ -5,4 +5,23 @@ class CategoryModel {
   final String description;
 
   CategoryModel({required this.id, required this.createdAt, required this.name, required this.description});
+
+   Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'createdAt': createdAt,
+      'name': name,
+      'description': description,
+    };
+  }
+
+  static CategoryModel fromMap(Map<String, dynamic> data) {
+    return CategoryModel(
+      id: data['id'] as String,
+      createdAt: data['createdAt'] as DateTime,
+      name: data['name'] as String,
+      description: data['description'] as String,
+    );
+  }
+  
 }
