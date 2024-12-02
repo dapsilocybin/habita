b4b3f7ff37a17b11751ef12a28fccf32c3907f8f
Mostafa Mashayekhi <dapsilocybin@gmail.com>
2024-12-02 11:07:55 +0330

    Define mappers in medal_model.dart

--- lib/data/models/medal_model.dart
+++ lib/data/models/medal_model.dart
@@ -6,4 +6,25 @@ class MedalModel {
   final String habitId;
 
   MedalModel({required this.id, required this.createdAt, required this.title, required this.description, required this.habitId});
+
+   Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'createdAt': createdAt,
+      'title': title,
+      'description': description,
+      'habitId': habitId,
+    };
+  }
+
+  static MedalModel fromMap(Map<String, dynamic> data) {
+    return MedalModel(
+      id: data['id'] as String,
+      createdAt: data['createdAt'] as DateTime,
+      title: data['title'] as String,
+      description: data['description'] as String,
+      habitId: data['habitId'] as String,
+    );
+  }
+  
 }
