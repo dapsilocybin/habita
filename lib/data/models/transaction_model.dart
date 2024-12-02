b4627b5e5232773c102a83617568f755bcaab902
Mostafa Mashayekhi <dapsilocybin@gmail.com>
2024-12-02 10:45:37 +0330

    define mappers in transaction_model.dart

--- lib/data/models/transaction_model.dart
+++ lib/data/models/transaction_model.dart
@@ -6,4 +6,24 @@ class TransactionModel {
   final String toUserId;
 
   TransactionModel({required this.id, required this.createdAt, required this.amount, required this.fromUserId, required this.toUserId});
+
+   Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'createdAt': createdAt,
+      'amount': amount,
+      'fromUserId': fromUserId,
+      'toUserId': toUserId,
+    };
+  }
+
+  static TransactionModel fromMap(Map<String, dynamic> data) {
+    return TransactionModel(
+      id: data['id'] as String,
+      createdAt: data['createdAt'] as DateTime,
+      amount: data['amount'] as String,
+      fromUserId: data['fromUserId'] as String,
+      toUserId: data['toUserId'] as String,      
+    );
+  }
 }
