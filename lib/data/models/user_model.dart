2ef8f4fe62d1821923f761bc5af46df7485025ca
Mostafa Mashayekhi <dapsilocybin@gmail.com>
2024-12-02 10:39:08 +0330

    define mappers in user_model.dart

--- lib/data/models/user_model.dart
+++ lib/data/models/user_model.dart
@@ -12,5 +12,34 @@ class UserModel {
 
   UserModel({required this.id, required this.username, required this.email, required this.bio, required this.createdAt, required this.firstName, required this.lastName, required this.profilePicture, required this.walletAddress, required this.authUserId});
 
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'username': username,
+      'email': email,
+      'bio': bio,
+      'createdAt': createdAt,
+      'firstName': firstName,
+      'lastName': lastName,
+      'profilePicture': profilePicture,
+      'walletAddress': walletAddress,
+      'authUserId': authUserId,
+    };
+  }
+
+  static UserModel fromMap(Map<String, dynamic> data) {
+    return UserModel(
+      id: data['id'] as String,
+      username: data['username'] as String,
+      email: data['email'] as String,
+      bio: data['bio'] as String,
+      createdAt: data['createdAt'] as DateTime,
+      firstName: data['firstName'] as String,
+      lastName: data['lastName'] as lastName,
+      profilePicture: data['profilePicture'] as String,
+      walletAddress: data['walletAddress'] as String,
+      authUserId: data['authUserId'] as String,
+    );
+  }
 
 }
