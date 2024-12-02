a34af26390c9aac6f4bde289a2f38070adb72e52
Mostafa Mashayekhi <dapsilocybin@gmail.com>
2024-12-02 11:15:27 +0330

    Define mappers in habit_model.dart

--- lib/data/models/habit_model.dart
+++ lib/data/models/habit_model.dart
@@ -13,4 +13,35 @@ class HabitModel {
   final String creatorId;
 
   HabitModel({required this.id, required this.createdAt, required this.title, required this.description, required this.stars, required this.goal, required this.isPublic, required this.totalMembers, required this.totalRecords, required this.frequencyId, required this.categoryId, required this.creatorId});
+
+   Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'createdAt': createdAt,
+      'title': title,
+      'description': description,
+      'stars': starts,
+      'goal': goal,
+      'isPublic': isPublic,
+      'totalMembers': totalMembers,
+      'totalRecords': totalRecords,
+      'creatorId': creatorId,
+    };
+  }
+
+  static HabitModel fromMap(Map<String, dynamic> data) {
+    return HabitModel(
+      id: data['id'] as String,
+      createdAt: data['createdAt'] as DateTime,
+      title: data['title'] as String,
+      description: data['description'] as String,
+      stars: data['stars'] as int,
+      goal: data['goal'] as String,
+      isPublic: data['isPublic'] as bool,
+      totalMembers: data['totalMembers'] as int,
+      totalRecords: data['totalRecords'] as int,
+      creatorId: data['creatorId'] as String,
+    );
+  }
+  
 }
